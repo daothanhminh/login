3ef3ab0ac203252bb84ef4c189bf279eb5cd9c79
linux-neoros <muhammadsalama909@gmail.com>
2023-01-28 19:01:53 +0700

    28 tháng 1 năm 2023

--- assets/js/signin.js
+++ assets/js/signin.js
@@ -1,3 +1,31 @@
+function setCookie(cname,cvalue) {
+    const d = new Date();
+    let time = 30*60*1000
+    d.setTime(d.getTime() + time);
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+  function eraseCookie(cname) {
+    document.cookie = cname+"=; expires = Thu, 01 Jan 1970 00:00:00 GMT"
+  }
+
   function checkInformation() {
     let $username = $('#id_username').val();
     let $password = $('#id_password').val();
@@ -6,7 +34,7 @@
       username: $('#id_username').val(),
       password: $('#id_password').val()
     };
-    if(localStorage.getItem("info") === null){ 
+    if(localStorage.getItem("info") === null){
       if($username == '' || $password == ''){
         $("#id_username").val("");
         $("#id_password").val("");
@@ -31,18 +59,39 @@
 
   function changePassword() {
     let $password = $('#id_new_password').val();
-    if(localStorage.getItem("info") === null){
+    if(getCookie('pass_changed') == 'true'){
       $('#id_new_password').val("");
       $(".dialog-auth-failed").show();
       $(".dialog-auth-failed p").text("Bạn không thể đổi mật khẩu ngay lúc này.");
-    } else if(localStorage.getItem("info") !== null){
-      let infoData = JSON.parse(localStorage.getItem("info"));
-      const info = {
-        username: infoData.username,
-        password: $('#id_new_password').val()
-      };
-      localStorage.setItem("info", JSON.stringify(info));
-      $("#id_new_password").val("");
-      window.location.href="https://baodaigov.github.io/login/index.html";
+    } else {
+      if($password == ''){
+        $('#id_new_password').val("");
+        $(".dialog-auth-failed").show();
+        $(".dialog-auth-failed p").text("Vui lòng nhập mật khẩu.");
+      } else {
+        if(localStorage.getItem("info") === null){
+          $('#id_new_password').val("");
+          $(".dialog-auth-failed").show();
+          $(".dialog-auth-failed p").text("Bạn không thể đổi mật khẩu ngay lúc này.");
+        } else if(localStorage.getItem("info") !== null){
+            let infoData = JSON.parse(localStorage.getItem("info"));
+            const info = {
+              username: infoData.username,
+              password: $password
+            };
+            localStorage.setItem("info", JSON.stringify(info));
+            $("#id_new_password").val("");
+            window.location.href="/index.html";
+            setCookie("pass_changed","true");
+          }
+        }
     }
   }
+
+window.onload = function passChanged(){
+  let passChanged = getCookie('pass_changed');
+  if(passChanged == 'true'){
+    $(".forgot-password-disable").show();
+    $(".forgot-password").hide();
+  }
+}
